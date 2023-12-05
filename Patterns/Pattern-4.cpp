454646e5b347d8fe7f97d146cc0fc22a3395ec3c
SanishKumar <18sanishkumar@gmail.com>
2023-12-05 20:54:06 +0530

    Alphabet Patterns!

--- Patterns/Pattern-4.cpp
+++ Patterns/Pattern-4.cpp
@@ -5,16 +5,13 @@ int main(){
     int n;
     cin >> n;
 
-    char ch = 'A';
 
     int row = 1;
     while(row <= n){
 
+        char ch = 'A' + row - 1;
         int col = 1;
         while(col <= n){
-            // char ch = 'A' + row -1;
-            // char ch = 'A' + col -1;
-            // char ch = 'A';
             cout << ch;
             ch = ch + 1;
             col = col + 1;
