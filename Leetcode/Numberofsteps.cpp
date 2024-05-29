027b46d95eee910a17ca5419b28a729371b5e1ba
SanishKumar <18sanishkumar@gmail.com>
2024-05-29 20:38:08 +0530

    1404. Number of Steps to Reduce a Number in Binary Representation to One

---
+++ Leetcode/Numberofsteps.cpp
@@ -0,0 +1,17 @@
+// 1404. Number of Steps to Reduce a Number in Binary Representation to One
+
+class Solution {
+public:
+    int numSteps(string s) {
+        int carry = 0, steps = 0;
+        for(int i = s.length() - 1; i > 0; i--) {
+            if((s[i] - '0') + carry == 1) {
+                steps += 2;
+                carry = 1;
+            } else {
+                steps++;
+            }
+        }
+        return steps + carry;
+    }
+};
