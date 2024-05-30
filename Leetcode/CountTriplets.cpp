641f7b3a578a21f70be311e0ecfb5e823e6ea475
SanishKumar <18sanishkumar@gmail.com>
2024-05-30 15:00:39 +0530

    1442. Count Triplets That Can Form Two Arrays of Equal XOR

---
+++ Leetcode/CountTriplets.cpp
@@ -0,0 +1,28 @@
+// 1442. Count Triplets That Can Form Two Arrays of Equal XOR
+
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int count = 0;
+        int n = arr.size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int a = 0;
+                for (int k = i; k < j; k++) {
+                    a ^= arr[k];
+                }
+                
+                int b = 0;
+                for (int k = j; k < n; k++) {
+                    b ^= arr[k];
+                    if (a == b) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+};
