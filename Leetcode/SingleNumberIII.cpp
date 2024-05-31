ab5fb0500dea9abe4f0e4d7daf8a41c1c5b0f8f7
SanishKumar <18sanishkumar@gmail.com>
2024-05-31 15:12:04 +0530

    260. Single Number III

---
+++ Leetcode/SingleNumberIII.cpp
@@ -0,0 +1,28 @@
+// 260. Single Number III
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> result(2, 0);
+        int index = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            bool found = false;
+            for (int j = 0; j < n; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result[index++] = nums[i];
+                if (index == 2) {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+};
