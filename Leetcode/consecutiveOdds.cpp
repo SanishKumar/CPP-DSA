68bb3bb599301f8e9dbfa00cad61a3edc0c0a5a7
SanishKumar <18sanishkumar@gmail.com>
2024-07-01 22:56:29 +0530

    1550. Three Consecutive Odds

---
+++ Leetcode/consecutiveOdds.cpp
@@ -0,0 +1,21 @@
+// 1550. Three Consecutive Odds
+
+class Solution {
+public:
+    static bool threeConsecutiveOdds(vector<int>& arr) {
+        int len=0;
+        for(int x: arr){
+            len=(x&1)?len+1:0;
+            if (len==3) return 1;
+        }
+        return 0;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
