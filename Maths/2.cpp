78d800de7f4ec039a60e03d375b3c5eb956a8d10
SanishKumar <18sanishkumar@gmail.com>
2024-07-12 23:50:58 +0530

    Pointers

---
+++ Maths/2.cpp
@@ -0,0 +1,19 @@
+// GCD
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    if(a==0){
+        return b;
+    }if(b==0){
+        return a;
+    }
+    while(a!=b){
+        
+    }
+}
+
+int main(){
+
+}
